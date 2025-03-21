4a1ab7a82b009e710fac94b395349c4781b3590e
Dushime Beni <bdushime47@gmail.com>
2025-03-21 09:52:43 +0200

    hamburger menu functionality

---
+++ src/scripts/header.js
@@ -0,0 +1,36 @@
+const initHeader = () => {
+  const hamburgerIcon = document.getElementById('hamburgerIcon');
+  const closeIcon = document.getElementById('closeIcon');
+  const closeButton = document.getElementById('closeButton');
+  const mobileMenu = document.getElementById('mobileMenu');
+
+  if (!hamburgerIcon || !closeIcon || !mobileMenu || !closeButton) {
+      console.error("Header elements not found!");
+      return;
+  }
+
+  // Open Menu
+  hamburgerIcon.addEventListener('click', function() {
+      console.log("Hamburger clicked");
+      mobileMenu.classList.remove('hidden');
+      hamburgerIcon.classList.add('hidden');
+      closeIcon.classList.remove('hidden');
+  });
+
+  // Close Menu
+  closeIcon.addEventListener('click', function() {
+      console.log("Close clicked");
+      mobileMenu.classList.add('hidden');
+      closeIcon.classList.add('hidden');
+      hamburgerIcon.classList.remove('hidden');
+  });
+
+  // Close menu with close button inside the menu
+  closeButton.addEventListener('click', function() {
+      mobileMenu.classList.add('hidden');
+      closeIcon.classList.add('hidden');
+      hamburgerIcon.classList.remove('hidden');
+  });
+};
+
+initHeader();
