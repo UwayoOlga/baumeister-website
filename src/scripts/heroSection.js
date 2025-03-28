2898b96ae815f2005b7f9a361db91e9d303688bd
Gdev <125574259+gisachris@users.noreply.github.com>
2025-03-28 13:58:21 +0200

    2 parent hero section

--- src/scripts/heroSection.js
+++ src/scripts/heroSection.js
@@ -3,8 +3,10 @@ function checkSectionPosition() {
     const rect = polysHolder.getBoundingClientRect();
     if (rect.top <= 0 && rect.bottom > 0) {
         polysHolder.classList.add("scrollable");
+        polysHolder.classList.add("overflow-auto");
     } else {
         polysHolder.classList.remove("scrollable");
+        polysHolder.classList.remove("overflow-auto");
     }
 }
 
