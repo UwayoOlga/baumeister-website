0992e4d986767eaa7e5e50aa93265a83b018fb4d
Gdev <125574259+gisachris@users.noreply.github.com>
2025-04-01 12:36:41 +0200

    implement mobile view responsiveness and functionality

--- src/scripts/heroSection.js
+++ src/scripts/heroSection.js
@@ -3,10 +3,8 @@ function checkSectionPosition() {
     const rect = polysHolder.getBoundingClientRect();
     if (rect.top <= 0 && rect.bottom > 0) {
         polysHolder.classList.add("scrollable");
-        polysHolder.classList.add("overflow-auto");
     } else {
         polysHolder.classList.remove("scrollable");
-        polysHolder.classList.remove("overflow-auto");
     }
 }
 
